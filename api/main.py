21abbd0ff2d92a6fd3eb82353302e63ebfc08671
aliff850 <aliff.ramly@gmail.com>
2026-06-01 18:06:08 +0800

    FEAT: Set it to run on the alternate tokenizer and 10 epoch model

--- api/main.py
+++ api/main.py
@@ -20,8 +20,9 @@ app = FastAPI(
     description="A FastAPI server running a bidirectional LSTM model to classify cyberbullying."
 )
 
-MODEL_PATH = os.getenv("MODEL_PATH", "optimized_nostop_bidirectional_lstm_model.keras")
-TOKENIZER_PATH = os.getenv("TOKENIZER_PATH", "keras_tokenizer.pkl")
+# MODEL_PATH = os.getenv("MODEL_PATH", "optimized_nostop_bidirectional_lstm_model.keras")
+MODEL_PATH = os.getenv("MODEL_PATH", "optimized_nostop_bidirectional_lstm_model_10epoch.keras")
+TOKENIZER_PATH = os.getenv("TOKENIZER_PATH", "keras_tokenizer_v2.pkl")
 
 # Initialize global predictor
 predictor = None
